77033390e32ee80a306fa3efcbc39f6b2c62dc8c
Ona Demuytere <onademuy@student.arteveldehs.be>
2020-08-22 15:24:00 +0200

    laatste

--- src/components/waterniveau/waterniveau.component.jsx
+++ src/components/waterniveau/waterniveau.component.jsx
@@ -1,38 +1,54 @@
 //import React, { useState } from 'react';
 import React from 'react';
 import './waterniveau.styles.scss';
+import firebase from '../../firebase';
 
 class Waterniveau extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            status: "filling", //okay, filling or something else
+            //status: null, //okay or something else
+            boodschappen: [],
+            stopzetten: null,
         }
+    }
+
+    componentDidMount() {
+        var boodschappen = [];
+        firebase
+        .firestore()
+        .collection("boodschap")
+        .get()
+        .then((querySnapshot) => {  //Notice the arrow funtion which bind `this` automatically.
+            querySnapshot.forEach(function(doc) {
+                boodschappen.push(doc.data());
+            });
+            this.setState({ boodschappen: boodschappen });   //set data in state here
+            //this.setState({bijvullen: this.state.boodschappen[0].manueleAanvraag})
+            this.setState({stopzetten: this.state.boodschappen[0].manueleStop})
+        });
     }
 
     render(){
         //make de error pinking
-        const {status} = this.state;
+        const {stopzetten} = this.state;
         console.log(this.state);
 
         return(
             <div>
-                    {status === "okay" ? 
+                    {stopzetten === false ? 
                         <div className='waterniveau green'>
-                            <div className="title">WATERNIVEAU</div>
-                            <div className="status">STATUS: in orde</div>
+                            <div className="title">WATERTOEVOER</div>
+                            <div className="status">STATUS: Systeem is ingeschakeld </div>
                         </div>
                     : 
                         <div className='waterniveau red'>
-                            <div className="title">WATERNIVEAU</div>
-                            <div className="statuswhite">ERROR: Controleer de aansluiting </div>
+                            <div className="title">WATERTOEVOER</div>
+                            <div className="statuswhite">STATUS: Systeem is uitgeschakeld </div>
 
                         </div> 
                     }
-
             </div>
-
-    
         );
     }
 }
